0a2bc248603def8d658885b45e7111604cec32ec
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-02 21:03:08 +0200

    Added a MongoDB Setup

--- API/server.js
+++ API/server.js
@@ -1,6 +1,19 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
+mongoose = require('mongoose');
+Task = require('./app/models/todolistModel'); // Model Loading Here
+bodyParser = require('body-parser');
+
+// Mongoose URL Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./app/routes/todolistRoutes'); // Importing routes
+routes(app); // Register the route
 
 app.listen(port);
 
