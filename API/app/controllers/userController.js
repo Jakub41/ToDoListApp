6fbdd8d1e715bc03b14c3c1aa1dc8cb11b12681c
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-03 22:55:46 +0200

    Added Register User

--- API/app/controllers/userController.js
+++ API/app/controllers/userController.js
@@ -14,3 +14,28 @@ exports.registerUser = registerUser;
 exports.loginUser = loginUser;
 exports.getUser = getUser;
 exports.logoutUser = logoutUser;
+
+function registerUser(request, reply) {
+    return new Promise((resolve, reject) => {
+        let userInfo = request.playload;
+        userInfo.access_token = uuidv1();
+        if (userInfo.password)
+            userInfo.password = bcrypt.hashSync(
+                userInfo.password,
+                Constants.saltRounds
+            );
+
+        User(userInfo).save()
+            .then(success => {
+                let response = {
+                    flag: statusCodes.CREATED,
+                    message: statusCodes.getStatusText(statusCodes.CREATED),
+                    access_token: success.access_token
+                }
+                return resolve(response);
+            })
+            .catch(error => {
+                return reject(error);
+            });
+    });
+}
