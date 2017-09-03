6be387142d510d73f436b5c6f9560e51c618ab20
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-03 17:04:59 +0200

    Configured Swagger PlugIn

---
+++ API/app/plugins/swagger.js
@@ -0,0 +1,39 @@
+// Swagger PlugIn
+'use strict';
+
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('inert');
+const Vision = require('vision');
+
+// Register Swagger
+exports.register = (server, options, next) => {
+
+    server.register([
+        Inert,
+        Vision,
+        {
+            'register': HapiSwagger,
+            'options': {
+                'info': {
+                    'title': 'ToDo RESTFul API'
+                },
+                'pathPrefixSize': 2,
+                'payloadType': 'json',
+                'jsonEditor': true,
+                'consumes': ['application/json', 'application/x-www-form-urlencoded'],
+                'produces': ['application/json', 'application/x-www-form-urlencoded']
+            }
+        }
+    ], (err) => {
+        if (err) {
+            Logger.winstonLogger.error(err);
+        } else {
+            console.log('hapi-swagger interface loaded')
+        }
+    });
+    next();
+};
+
+exports.register.attributes = {
+    name: 'swagger-plugin'
+};
