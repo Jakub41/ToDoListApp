a36258153e4950ce5c56ddd529ef76e40aef28da
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-04 00:03:39 +0200

    Update Login User

--- API/app/controllers/userController.js
+++ API/app/controllers/userController.js
@@ -15,6 +15,11 @@ exports.loginUser = loginUser;
 exports.getUser = getUser;
 exports.logoutUser = logoutUser;
 
+/**
+ * Register user
+ * @param  {function} request
+ * @param  {function} reply
+ */
 function registerUser(request, reply) {
     return new Promise((resolve, reject) => {
         let userInfo = request.playload;
@@ -37,5 +42,65 @@ function registerUser(request, reply) {
             .catch(error => {
                 return reject(error);
             });
+    });
+}
+
+/**
+ * Login user using email or username, and password
+ * @param  {function} request
+ * @param  {function} reply
+ */
+function loginUser(request, reply) {
+    return new Promise((resolve, reject) => {
+        let payload = request.payload;
+        runner()
+            .then(response => {
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error);
+            });
+
+        async function runner() {
+            let filters = {};
+            if (payload.username)
+                filters.username = payload.username;
+
+            if (payload.email)
+                filters.email = payload.email;
+
+            let userArr = await User.find(filters);
+            if (userArr.length === 0) {
+                let response = {
+                    flag: statusCodes.NOT_FOUND,
+                    message: statusCodes.getStatusText(statusCodes.NOT_FOUND),
+                    description: 'Username or email does not exist'
+                }
+                return response;
+            }
+            let user = userArr[0];
+            if (user.password) {
+                let checkPassword = bcrypt.compareSync(payload.password, user.password);
+                if (!checkPassword) {
+                    let response = {
+                        flag: statusCodes.NOT_ACCEPTABLE,
+                        message: statusCodes.getStatusText(statusCodes.NOT_ACCEPTABLE),
+                        description: 'Password did not match'
+                    };
+                    return response;
+                }
+            }
+            user.access_token = uuidv1();
+            let updateInfo = {
+                access_token: user.access_token
+            }
+            await User.findOneAndUpdate(filters, updateInfo)
+            let response = {
+                flag: statusCodes.OK,
+                message: statusCodes.getStatusText(statusCodes.OK),
+                user: user
+            };
+            return response;
+        }
     });
 }
