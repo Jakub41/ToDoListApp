f8c56eca6d3588da8f90d771cafe40e4c7492606
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-03 21:15:32 +0200

    Added const & exports

--- API/app/controllers/userController.js
+++ API/app/controllers/userController.js
@@ -1,1 +1,16 @@
+// User Controller
 'use strict';
+
+const uuidv1 = require('uuid/v1');
+// For hash passwords - security
+const bcrypt = require('bcrypt');
+const utils = require('../../utils');
+const User = require('../models/userModel');
+const Logger = require('../lib/logger');
+const Constants = require('../config/constants');
+const statusCodes = require('../config/statusCodes');
+
+exports.registerUser = registerUser;
+exports.loginUser = loginUser;
+exports.getUser = getUser;
+exports.logoutUser = logoutUser;
