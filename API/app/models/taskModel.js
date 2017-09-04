5369833c0ad3199a021e49101873ced287f36c2d
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-04 22:41:26 +0200

    Model for Task Done

---
+++ API/app/models/taskModel.js
@@ -0,0 +1,38 @@
+// Model for Task
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const task = new Schema({
+    userId: {
+        type: Schema.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false,
+        default: null
+    },
+    dueOn: {
+        type: Date,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+}, {
+    timestamps: true // inserts createdAt and updatedAt
+});
