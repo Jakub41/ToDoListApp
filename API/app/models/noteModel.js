68799b967c1b72c9cf7d22db9f838f5ad65a786a
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-04 22:55:35 +0200

    Added a note model

---
+++ API/app/models/noteModel.js
@@ -0,0 +1,31 @@
+// Note model related to Task
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const note = new Schema({
+    taskId: {
+        type: Schema.ObjectId,
+        ref: 'task',
+        required: true
+    },
+    note: {
+        type: String,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    isDeleted: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+}, {
+    timestamps: true //inserts createdAt and updatedAt
+});
+
+module.exports = mongoose.model('note', note);
