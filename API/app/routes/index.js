1c110294da35d8c02c55a75dc086408fb438c401
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-05 21:39:18 +0200

    Routes modification for Task &  Note

--- API/app/routes/index.js
+++ API/app/routes/index.js
@@ -1,5 +1,7 @@
 'use strict';
-let UserRoute = require('./todolistRoutes');
+let UserRoute = require('./userRoute');
+let TaskRoute = require('./taskRoute');
+let NoteRoute = require('./noteRoute');
 
-let all = [].concat(UserRoute);
+let all = [].concat(UserRoute, TaskRoute, NoteRoute);
 module.exports = all;
