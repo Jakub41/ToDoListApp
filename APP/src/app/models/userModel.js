8d1eb194a7877c8297bc98ec7f99d8f7b79eb990
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-07 23:20:05 +0200

    Added User & Task Model

---
+++ APP/src/app/models/userModel.js
@@ -0,0 +1,24 @@
+const UserModel = Backbone.Model.extend({
+
+    defaults: {
+        first_name: '',
+        last_name: '',
+        username: '',
+        email: '',
+        password: '',
+    },
+
+    url: 'user',
+
+    validation: {
+        username: {
+            required: true,
+        },
+        password: {
+            required: true,
+            minLength: 6,
+        }
+    }
+});
+
+export default UserModel;
