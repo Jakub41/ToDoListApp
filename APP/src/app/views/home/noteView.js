72efd571836b213f3457cf6b461fe7d11b0576f2
Jakub Lemiszewski <jakublemiszewski@MacBook-Pro-di-jakub-2.local>
2017-09-15 14:43:20 +0200

    Minor fixing

--- APP/src/app/views/home/noteView.js
+++ APP/src/app/views/home/noteView.js
@@ -3,117 +3,117 @@ import Api from '../../api';
 
 // notes list view
 const NoteView = Mn.View.extend({
-  tagName: 'li',
-  className: 'note-item',
-  template: _.template(Template),
+    tagName: 'li',
+    className: 'note-item',
+    template: _.template(Template),
 
-  // data bindings
-  bindings: {
-    ':el': {
-      classes: {
-        'done': {
-          observe: 'isCompleted',
-        }
-      },
+    // data bindings
+    bindings: {
+        ':el': {
+            classes: {
+                'done': {
+                    observe: 'isCompleted',
+                }
+            },
+        },
+        '.note': 'note',
+        '.edit-note-form #note': 'note',
     },
-    '.note': 'note',
-    '.edit-note-form #note': 'note',
-  },
 
-  // view behaviour bindings
-  viewBindings: {
-    '.edit': {
-      observe: 'editNoteIsVisible',
-      visible: (val) => val,
+    // view behaviour bindings
+    viewBindings: {
+        '.edit': {
+            observe: 'editNoteIsVisible',
+            visible: (val) => val,
+        },
+        '.info': {
+            observe: 'editNoteIsVisible',
+            visible: (val) => !val,
+        },
     },
-    '.info': {
-      observe: 'editNoteIsVisible',
-      visible: (val) => !val,
+
+
+    // events bidings
+    events: {
+        'click .edit-note': 'toggleEditNote',
+        'click .edit-note-form .cancel': 'closeEditTask',
+        'click .remove-note': 'removeNote',
+        'click .complete-note': 'toggleCompleteNote',
+        'submit .edit-note-form': 'editNote',
     },
-  },
 
+    initialize() {
+        // init view managing model for bindings
+        this.viewModel = new Backbone.Model({
+            editNoteIsVisible: false,
+        });
 
-  // events bidings
-  events: {
-    'click .edit-note': 'toggleEditNote',
-    'click .edit-note-form .cancel': 'closeEditTask',
-    'click .remove-note': 'removeNote',
-    'click .complete-note': 'toggleCompleteNote',
-    'submit .edit-note-form': 'editNote',
-  },
+        // save note cache for edit undo
+        this.noteCache = this.model.toJSON();
+    },
 
-  initialize() {
-    // init view managing model for bindings
-    this.viewModel = new Backbone.Model({
-      editNoteIsVisible: false,
-    });
+    toggleEditNote() {
+        this.viewModel.set('editNoteIsVisible', !this.viewModel.get('editNoteIsVisible'));
+    },
 
-    // save note cache for edit undo
-    this.noteCache = this.model.toJSON();
-  },
+    closeEditTask() {
+        this.model.set(this.noteCache);
+        this.viewModel.set('editNoteIsVisible', false);
+    },
 
-  toggleEditNote() {
-    this.viewModel.set('editNoteIsVisible', !this.viewModel.get('editNoteIsVisible'));
-  },
+    // remove note. save as task
+    removeNote() {
+        Api.put('note', {
+            taskId: this.model.get('taskId'),
+            noteId: this.model.get('_id'),
+            isDeleted: true
+        }).then(res => {
+            if (res.flag === 202) {
+                this.model.destroy();
+            } else {
+                new Noty({ text: res.message, type: 'error' }).show();
+            }
+        }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
+    },
 
-  closeEditTask() {
-    this.model.set(this.noteCache);
-    this.viewModel.set('editNoteIsVisible', false);
-  },
+    // toggle note complete// save as task
+    toggleCompleteNote() {
+        Api.put('note', {
+            taskId: this.model.get('taskId'),
+            noteId: this.model.get('_id'),
+            isCompleted: !this.model.get('isCompleted')
+        }).then(res => {
+            if (res.flag === 202) {
+                this.model.set('isCompleted', !this.model.get('isCompleted'));
+            } else {
+                new Noty({ text: res.message, type: 'error' }).show();
+            }
+        }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
+    },
 
-  // remove note. save as task
-  removeNote() {
-    Api.put('note', {
-        taskId: this.model.get('taskId'),
-        noteId: this.model.get('_id'),
-        isDeleted: true
-      }).then(res => {
-        if (res.flag === 202) {
-          this.model.destroy();
-        } else {
-          new Noty({ text: res.message, type: 'error' }).show();
-        }
-      }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
-  },
+    // note edit submit function same as task submit
+    editNote(e) {
+        e.preventDefault();
 
-  // toggle note complete// save as task
-  toggleCompleteNote() {
-    Api.put('note', {
-        taskId: this.model.get('taskId'),
-        noteId: this.model.get('_id'),
-        isCompleted: !this.model.get('isCompleted')
-      }).then(res => {
-        if (res.flag === 202) {
-          this.model.set('isCompleted', !this.model.get('isCompleted'));
-        } else {
-          new Noty({ text: res.message, type: 'error' }).show();
-        }
-      }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
-  },
+        Api.put('note', {
+            taskId: this.model.get('taskId'),
+            noteId: this.model.get('_id'),
+            note: this.model.get('note'),
+        }).then(res => {
+            if (res.flag === 202) {
+                new Noty({ text: res.description, type: 'success' }).show();
+                this.noteCache = this.model.toJSON();
+                this.closeEditTask();
+            } else {
+                new Noty({ text: res.message, type: 'error' }).show();
+            }
+        }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
+    },
 
-  // note edit submit function same as task submit
-  editNote(e) {
-    e.preventDefault();
-
-    Api.put('note', {
-        taskId: this.model.get('taskId'),
-        noteId: this.model.get('_id'),
-        note: this.model.get('note'),
-      }).then(res => {
-        if (res.flag === 202) {
-          new Noty({ text: res.description, type: 'success' }).show();
-          this.noteCache = this.model.toJSON();
-          this.closeEditTask();
-        } else {
-          new Noty({ text: res.message, type: 'error' }).show();
-        }
-      }, e => new Noty({ text: e.responseJSON.message, type: 'error' }).show());
-  },
-
-  onRender() {
-    this.stickit();
-    this.stickit(this.viewModel, this.viewBindings);
-  }
+    onRender() {
+        this.stickit();
+        this.stickit(this.viewModel, this.viewBindings);
+    }
 });
 
 export default NoteView;
--- APP/src/app/views/register/index.js
+++ APP/src/app/views/register/index.js
@@ -3,74 +3,74 @@ import App from '../../main';
 import UserModel from '../../models/user';
 
 const RegisterView = Mn.View.extend({
-  className: 'register-page',
-  template: _.template(Template),
-  model: new UserModel(),
+    className: 'register-page',
+    template: _.template(Template),
+    model: new UserModel(),
 
-  // events bindings
-  events: {
-    'submit form': 'register',
-  },
+    // events bindings
+    events: {
+        'submit form': 'register',
+    },
 
-  // data bindings
-  bindings: {
-    '#first_name': 'first_name',
-    '#last_name': 'last_name',
-    '#email': 'email',
-    '#username': 'username',
-    '#password': 'password',
-  },
+    // data bindings
+    bindings: {
+        '#first_name': 'first_name',
+        '#last_name': 'last_name',
+        '#email': 'email',
+        '#username': 'username',
+        '#password': 'password',
+    },
 
-  initialize() {
-    // adding validation rules for register page
-    this.model.validation = $.extend(true, {}, this.model.validation, {
-      email: {
-        required: true,
-        pattern: 'email',
-      },
-      first_name: {
-        required: true,
-      },
-      last_name: {
-        required: true,
-      },
-    });
+    initialize() {
+        // adding validation rules for register page
+        this.model.validation = $.extend(true, {}, this.model.validation, {
+            email: {
+                required: true,
+                pattern: 'email',
+            },
+            first_name: {
+                required: true,
+            },
+            last_name: {
+                required: true,
+            },
+        });
 
-    // init validation
-    Backbone.Validation.bind(this);
-  },
+        // init validation
+        Backbone.Validation.bind(this);
+    },
 
-  register(e) {
-    e.preventDefault();
+    register(e) {
+        e.preventDefault();
 
-    // check form
-    if (this.model.isValid(true)) {
-      this.model.save()
-      .then(res => {
-        if (res.flag === 201) {
-          // login there
-          localStorage.setItem('access_token', res.access_token);
+        // check form
+        if (this.model.isValid(true)) {
+            this.model.save()
+                .then(res => {
+                    if (res.flag === 201) {
+                        // login there
+                        localStorage.setItem('access_token', res.access_token);
 
-          // show message
-          new Noty({ text: 'Welcome on board', type: 'success' }).show();
+                        // show message
+                        new Noty({ text: 'Welcome on board', type: 'success' }).show();
 
-          // navigate to home screen
-          App.navigate('');
-        } else {
-          // error. show message
-          new Noty({ text: res.message, type: 'error' }).show();
+                        // navigate to home screen
+                        App.navigate('');
+                    } else {
+                        // error. show message
+                        new Noty({ text: res.message, type: 'error' }).show();
+                    }
+                }, e => {
+                    new Noty({ text: e.responseJSON.message, type: 'error' }).show();
+                    console.log('error', e.responseJSON.statusCode, e.responseJSON.message);
+                });
         }
-      }, e => {
-          new Noty({ text: e.responseJSON.message, type: 'error' }).show();
-          console.log('error', e.responseJSON.statusCode, e.responseJSON.message);
-      });
-    }
-  },
+    },
 
-  onRender() {
-    // init bindings
-    this.stickit();
-  },
+    onRender() {
+        // init bindings
+        this.stickit();
+    },
 });
 
 export default RegisterView;
